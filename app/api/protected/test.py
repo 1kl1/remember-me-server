2ba2adc98f71201abcb2c1d9a15c3ca26fa847fd
1kl1 <mamy0320@naver.com>
2025-05-17 22:21:56 +0900

    document 추가

--- app/api/protected/test.py
+++ app/api/protected/test.py
@@ -1,30 +1,43 @@
-from typing import Any
-from fastapi import APIRouter, Depends
+from typing import Any, List
+from fastapi import APIRouter, Depends, HTTPException
 from langchain_chroma import Chroma
 from requests import Session
 from app.core.exceptions import NotFound
 from app.db.crud.user import get_user_by_firebase_uid
 from app.dependencies import get_current_user, get_db, get_vectordb
-from app.schemas.ai import QueryInput
+from app.schemas.ai import DocumentInput, QueryInput
 from app.schemas.auth import TokenData
 from app.schemas.user import User
 from langchain.chains import RetrievalQA
 from app.ai.base import llm
+from langchain_core.documents import Document
 
 
 router = APIRouter(prefix="/test", tags=["test"])
 
+@router.post("/add_documents/{collection_name}")
+def add_documents(
+    documents: List[DocumentInput],
+    vectordb: Chroma = Depends(get_vectordb),
+    ):
+    try:
+        langchain_docs = [
+            Document(page_content=doc.text, metadata=doc.metadata)
+            for doc in documents
+        ]
+
+        vectordb.add_documents(langchain_docs)
+        
+        return {"message": f"Added {len(documents)} documents to collection"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to add documents: {str(e)}")
+
+
 @router.post("/me")
 async def read_current_user(
     query_input: QueryInput,
-    # token_data: TokenData = Depends(get_current_user),
-    db: Session = Depends(get_db),
     vectordb: Chroma = Depends(get_vectordb),
 ) -> Any:
-    """
-    현재 로그인한 사용자 정보 조회
-    """
-
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
--- app/schemas/ai.py
+++ app/schemas/ai.py
@@ -1,4 +1,9 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class QueryInput(BaseModel):
     query: str
+
+class DocumentInput(BaseModel):
+    text: str
+    metadata: Optional[dict] = {}
